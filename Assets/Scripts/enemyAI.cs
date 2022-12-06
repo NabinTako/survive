e95b7f81f4bca76518795d1e987d2dc627847281
NabinTako <nabintako999@gmail.com>
2022-12-06 20:43:52 +0545

    improved systems + animations

--- Assets/Scripts/enemyAI.cs
+++ Assets/Scripts/enemyAI.cs
@@ -7,8 +7,9 @@ public class enemyAI : MonoBehaviour
 {
     // Start is called before the first frame update
     //You may consider adding a rigid body to the zombie for accurate physics simulation
-    [SerializeField]
     private GameObject player;
+    [SerializeField]
+    private GameObject deathAnimation;
     private Vector3 playerPosition;
     private Vector3 Scale;
     //This will be the zombie's speed. Adjust as necessary.
@@ -17,56 +18,66 @@ public class enemyAI : MonoBehaviour
 
     [SerializeField]
     private int attackPower = 5;
+    private float objectScale;
+
+    [SerializeField]
+    private int HP = 100;
 
 
-    private bool isLook = true;
+    public bool isPlayerAlive = true;
     void Start()
     {
         //At the start of the game, the zombies will find the gameobject called wayPoint.
         player = GameObject.Find("player");
+        Scale = transform.localScale;
+        objectScale = transform.localScale.x;
+        StartCoroutine(leftRight());
     }
 
     void Update()
     {
-        playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        if (isPlayerAlive)
+        {
+            playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        }
         //Here, the zombie's will follow the waypoint.
         transform.position = Vector3.MoveTowards(transform.position, playerPosition, speed * Time.deltaTime);
 
-        Scale = transform.localScale;
-        Vector3 checkPos = transform.position * Time.deltaTime; 
-        if (checkPos.x > transform.position.x && isLook)
+        if(HP < 0)
         {
-            change();
+            Instantiate(deathAnimation,transform.position,Quaternion.identity);
+            Destroy(this.gameObject);
         }
 
     }
-
-    void change()
+    public void takeDmg(int attackPower)
     {
-        Scale.x *= -1;
-        transform.localScale = Scale;
-        isLook = false;
-        /*
-        if (isLook == true)
-        {
-            Vector3 checkPos = transform.position * Time.deltaTime;
-            if (checkPos.x > transform.position.x)
-            {
-                isLook = false;
-            }
-            else
-            {
-                //Scale.x *= -1;
-            }
-        }
-        */
+        HP -= attackPower;
     }
     public IEnumerator doDamage()
     {
         movement player = this.player.GetComponent<movement>();
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(0.5f);
         player.takedamage(attackPower);
         StopCoroutine("doDamage");
+    }
+    IEnumerator leftRight() //  makes enemies look to left or right.
+    {
+         while (true)
+        {
+            Vector3 checkPos = transform.position;
+            yield return new WaitForSeconds(0.2f);
+            if (checkPos.x > transform.position.x)
+            {
+                Scale.x = -objectScale;
+                transform.localScale = Scale;
+            }
+            else
+            {
+                Scale.x = objectScale;
+                transform.localScale = Scale;
+        }
+        }
     }
 
 }
--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -6,62 +6,109 @@ using UnityEngine;
 public class movement : MonoBehaviour
 {
     Vector3 playerPosition;
+    Vector3 playerScale;
     [SerializeField]
     int life = 100;
     int speed = 5;
+
+    bool isAlive = true;
     [SerializeField]
     GameObject enemy;
+    [SerializeField]
+    GameObject playerDied;
+
+
+    Animator animate;
 
     enemyAI enemyAI;
+    GameObject[] EveryEnemies;
     // Start is called before the first frame update
     void Start()
     {
         playerPosition = transform.position;
-        enemyAI = GameObject.FindGameObjectWithTag("enemy").GetComponent<enemyAI>();
-       // StartCoroutine(spanEnemys());
+        playerScale = transform.localScale;
+        StartCoroutine(spanEnemys());
+        animate = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        EveryEnemies = GameObject.FindGameObjectsWithTag("enemy");
+
+        if (life < 0)
+        {
+            
+            isAlive = false;
+            foreach (GameObject enemy in EveryEnemies)
+            {
+                enemy.GetComponent<enemyAI>().isPlayerAlive = false;
+            }
+            Instantiate(playerDied,transform.position,Quaternion.identity);
+            Destroy(this.gameObject);
+        }
         if (Input.GetKey(KeyCode.W))
         {
             transform.position += Vector3.up * speed * Time.deltaTime;
+            animate.SetBool("isRunning", true);
         }
-        if (Input.GetKey(KeyCode.S))
+        else if (Input.GetKey(KeyCode.S))
         {
             transform.position += Vector3.down * speed * Time.deltaTime;
+            animate.SetBool("isRunning", true);
         }
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
             transform.position += Vector3.left * speed * Time.deltaTime;
+            animate.SetBool("isRunning", true);
+            playerScale.x = -3;
+            transform.localScale = playerScale;
         }
-        if (Input.GetKey(KeyCode.D))
+        else if (Input.GetKey(KeyCode.D))
         {
             transform.position += Vector3.right * speed * Time.deltaTime;
+            animate.SetBool("isRunning", true);
+            playerScale.x = 3;
+            transform.localScale = playerScale;
+        }
+        else
+        {
+            animate.SetBool("isRunning", false);
+
         }
        
     }
     private void OnCollisionStay2D(Collision2D collision)
     {
-        Debug.Log(collision.gameObject.name);
+
+        enemyAI = collision.gameObject.GetComponent<enemyAI>();
+        //Debug.Log(collision.gameObject.name);
         if (collision.gameObject.tag == "enemy")
         {
             enemyAI.StartCoroutine("doDamage");
+            StartCoroutine("giveDmg");
         }
     }
     public void takedamage(int dmgValue)
     {
         life -= dmgValue;
     }
+    // 
     IEnumerator spanEnemys()
     {
-        while (true)
+        while (isAlive)
         {
             yield return new WaitForSeconds(2.0f);
             playerPosition = new Vector3(transform.position.x + Random.Range(5f,10f), transform.position.y + Random.Range(5f, 10f), transform.position.z );
             Instantiate(enemy, playerPosition, Quaternion.identity);
         }
+    }
+
+    IEnumerator giveDmg()
+    {
+        yield return new WaitForSeconds(0.5f);
+        enemyAI.takeDmg(50);
+        StopCoroutine("giveDmg");
     }
 }
 
